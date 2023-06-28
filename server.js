929b73e94a7c13ad6e62704b81dafa0632b94a4a
jayhee3 <jiayuanhee@gmail.com>
2023-06-29 00:49:41 +0800

    Fix Heroku port number issue

---
+++ server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import axios from "axios";
+import cors from "cors";
+const {Request, Response} = express;
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors()); // Enable CORS for all routes
+
+// Endpoint to proxy the Google Places API request
+app.get("/api/nearby-restaurants", async (req, res) => {
+  const {latitude, longitude} = req.query;
+  const apiKey = "AIzaSyBxPOata2rKGBKtqGI53U4DPfAoE_H4Hcw"; // Replace with your actual API key
+  const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&key=${apiKey}`;
+
+  try {
+    const response = await axios.get(url);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching nearby restaurants:", error);
+    res.status(500).json({error: "An error occurred"});
+  }
+});
+
+app.get("/api/search-restaurants", async (req, res) => {
+  const {keyword, latitude, longitude} = req.query;
+  const apiKey = "AIzaSyBxPOata2rKGBKtqGI53U4DPfAoE_H4Hcw"; // Replace with your actual API key
+  const url_search = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${latitude},${longitude}&radius=1500&type=restaurant&keyword=${keyword}&key=${apiKey}`;
+  console.log(url_search);
+
+  try {
+    const response = await axios.get(url_search);
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error searching for restaurants:", error);
+    res.status(500).json({error: "An error occurred"});
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
